c1a184b371ac1d993de9935923ee0492d31a2693
jake-is-ESD-protected <jt@tschavoll.at>
2023-11-23 14:53:29 +0100

    structure verbose prints, add progress bar

--- seedcli/seedcli.py
+++ seedcli/seedcli.py
@@ -14,7 +14,6 @@ class CSeedCli:
             print(printable)
 
     def getDaisyPort(self):
-        DAISY_HW_IDENTIFIER = "VID:PID=0483:5740"
         ports = list(list_ports.comports())
         for port in ports:
             if DAISY_HW_IDENTIFIER in port.hwid:
@@ -31,27 +30,43 @@ class CSeedCli:
 
     def __periodicTransceive(self, msg: str, waitTime: float = 0.02):
         fileSize = len(msg)
-        fullBlockSize = BLOCKSIZE + len(CMD_DATA)
         blockI = 0
-        blocks = (fileSize / fullBlockSize).__ceil__()
+        blocks = (fileSize / BLOCKSIZE).__ceil__()
         ser = serial.Serial(self.getDaisyPort())
+        crcData = bytes()
         while (blockI < blocks):
             self.__vPrint(
-                f"writing {BLOCKSIZE + len(CMD_DATA)} bytes to {ser.port}: Block #{blockI+1}/{blocks}")
+                f"writing {BLOCKSIZE + len(CMD_DATA)} bytes to {ser.port}: Block #{blockI+1}/{blocks}:")
             split = msg[blockI*BLOCKSIZE:(blockI+1)*BLOCKSIZE]
             cmdData = commands.CcmdData.getInstance(split)
-            self.__vPrint(cmdData.parse().encode())
-            ser.write(cmdData.parse().encode())
+            dataRaw = cmdData.parse().encode()
+            self.__vPrint(dataRaw)
+            crcData += dataRaw[len(CMD_DATA):]
+            ser.write(dataRaw)
             sleep(waitTime)
             stat = ser.readline().decode()
             stat = stat.replace("\n", "").replace("\r", "")
             if stat != (CLI_PREFIX + " " + RESPONSE_OK):
-                self.__vPrint(f"Early exit error: {stat}")
+                print(f"Early exit error: {stat}")
                 return stat
-            print(stat)
+            self.__vPrint(stat)
             blockI += 1
-        # TODO: CRC
-        return CLI_PREFIX + " " + RESPONSE_OK
+            self.progressBar(0, blocks)
+        crc = self.crc16(crcData)
+        return CLI_PREFIX + " " + RESPONSE_OK, crc
+
+    def crc16(self, data):
+        crc = CRC_INITIAL_VALUE
+        for byte in data:
+            crc ^= byte
+            for _ in range(8):
+                crc = (crc >> 1) ^ CRC_POLYNOMIAL if crc & 1 else crc >> 1
+        return crc & 0xFFFF
+
+    def progressBar(self, min, max):
+        if self.verbose:
+            return
+        print(f"{'█' * int(100/(max-min))}", end="", flush=True)
 
     def run(self):
         cmd = commands.Ccmd.fromTerminal()
@@ -60,29 +75,37 @@ class CSeedCli:
             return
         stat = cli.__oneShotTransceive(cmd.parse())
         self.__vPrint(stat)
-        if stat.startswith(CLI_PREFIX + " " + RESPONSE_OK):
+
+        if RESPONSE_OK in stat:
             return
-        if stat.startswith(CLI_PREFIX + " " + RESPONSE_ERR):
+        if RESPONSE_ERR in stat:
             print(f"Error: Seed returned {stat}")
             return
         with open(cmd.targetFile, "r") as f:
             data = f.read()
-        stat = self.__periodicTransceive(data)
-        self.__vPrint(stat)
-        if not stat.startswith(CLI_PREFIX + " " + RESPONSE_OK):
+        print(f"Uploading <{f.name}> to Seed...")
+        stat, crc = self.__periodicTransceive(data)
+        if RESPONSE_OK not in stat:
             print(f"Error: A block transfer failed")
             return
+
         cmdStop = commands.CcmdStop.getInstance()
         stat = self.__oneShotTransceive(cmdStop.parse())
         self.__vPrint(stat)
-        if not stat.startswith(CLI_PREFIX + " " + RESPONSE_FNSH):
+        if RESPONSE_FNSH not in stat:
             print("Seed did not terminate connection with properly!")
             return
+        self.__vPrint(f"CRC checksum on client side: {crc}")
+        if not stat.replace("\n", "").replace("\r", "").endswith(str(crc)):
+            print("CRC checksums do not match, data might be corrupted!")
+            return
+        print()
+        print(stat)
 
 
 if __name__ == "__main__":
     # from unittest import mock
     # onTerminal = "seedcli send ./seedcli/tests/data_big.txt --sdram"
     # with mock.patch('sys.argv', onTerminal.split(" ")):
-    cli = CSeedCli(verbose=True)
+    cli = CSeedCli(verbose=False)
     cli.run()
